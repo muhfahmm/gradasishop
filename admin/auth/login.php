e2078bd097895b62c8785ceba94d3c14b888026e
muhfahmm <148422221+muhfahmm@users.noreply.github.com>
2025-11-09 13:15:11 +0700

    gradasishop

---
+++ admin/auth/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="api/login.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">Login</button>
+        <a href="register.php">Register</a>
+    </form>
+</body>
+</html>
+
+
+
+
+
+
+
+
