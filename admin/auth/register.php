33f691f7ea33d4ac11634d9a77edd530551b961d
muhfahmm <148422221+muhfahmm@users.noreply.github.com>
2025-11-09 13:14:50 +0700

    gradasishop

---
+++ admin/auth/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <form action="api/process-register.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">Register</button>
+        <a href="login.php">Login</a>
+    </form>
+</body>
+</html>
